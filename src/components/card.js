a824c0d50d6ba46231583b3c967a0224ddfdd8bf
Thor Denson <tjdenson@eckerd.edu>
2018-07-30 23:57:45 -0400

    created props in app-main for card.js

---
+++ src/components/card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { prependOnceListener } from 'cluster';
+
+
+class Card extends Component {
+
+    render() {
+        let data = this props.data
+        // if movie ID found, then...
+
+
+
+
+
+        // conditional statement for no data
+    }
+}
