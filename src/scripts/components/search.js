30385e9a03c340eb1bc088214c3b6dfda0b98db7
Thor Denson <tjdenson@eckerd.edu>
2018-07-31 23:17:05 -0400

    tidied spacing

--- src/scripts/components/search.js
+++ src/scripts/components/search.js
@@ -4,27 +4,27 @@ const TMDBLogo = 'https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78
 
 class SearchBox extends Component {
 
-  handleChange(event) {
-    event.target.select();
-  }
-  render() {
-    return (
-      <div className="col-xs-12 search-container nopadding">
-        <div className="row">
+    handleChange(event) {
+        event.target.select();
+        }
+        render() {
+            return (
+                <div className="col-xs-12 search-container nopadding">
+                    <div className="row">
 
-          <div className="col-xs-12 col-sm-6 col-lg-5">
-            <a href="./" title="ReactJS TMDb Movie Search"><img src={TMDBLogo} className="logo" alt="The Movie Database" /></a>
-          </div>
-          <div className="col-xs-12 col-sm-6 col-lg-7">
-            <form className="searchbox">
-              {/* <label> */}
-                <input ref="search suggestion" onClick={this.handleChange} className="searchbox__input typeahead form-control" type="text" placeholder="Search Movie Title..." id="q" />
-              {/* </label> */}
-              </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
+                        <div className="col-xs-12 col-sm-6 col-lg-5">
+                            <a href="./" title="ReactJS TMDb Movie Search"><img src={TMDBLogo} className="logo" alt="The Movie Database" /></a>
+                        </div>
+                        <div className="col-xs-12 col-sm-6 col-lg-7">
+                            <form className="searchbox">
+                                {/* <label> */}
+                                    <input ref="search suggestion" onClick={this.handleChange} className="searchbox__input typeahead form-control" type="text" placeholder="Search Movie Title..." id="q" />
+                                {/* </label> */}
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
 }
 module.exports = SearchBox;
